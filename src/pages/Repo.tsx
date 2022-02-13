ff23f987db5868346b3980a42000dd6f95dc216c
Joao Gomes <joaopfg.2002@gmail.com>
2022-02-13 23:24:02 +0000

    Update without refresh :tada: Stale While Revalidate / Scale Time :tada:

--- src/pages/Repo.tsx
+++ src/pages/Repo.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useQueryClient } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { Repository } from './Repos';
 
-export function  Repo() {
+export function Repo() {
+  const params = useParams()
+  const currentRepository = params['*'] as string
+
+  const queryClient = useQueryClient();
+
+  async function handleChangeRepositoryDescription() {
+    // Update description by api request
+    const previousRepos = await queryClient.getQueryData<Repository[]>('repos')
+
+    if (previousRepos) {
+      const nextRepos = previousRepos.map( repo => {
+        if (repo.full_name === currentRepository) {
+          return { ...repo, description: 'Testing' }
+        } else {
+          return repo
+        }
+      })
+
+      queryClient.setQueriesData('repos', nextRepos)
+      
+    }
+  }
+
   return (
-    <div>
-        <h1>Hello World</h1>
-    </div>
+ <div>
+    <h1>{ currentRepository }</h1>
+      <button
+        onClick={handleChangeRepositoryDescription}
+      >Change repository description</button>
+
+  </div>
+    
   );
 }
